ff8f81de60a5207405d51c0c0453576c7dbfb077
Marius Agerup <Marius.schei@outlook.com>
2022-10-16 01:14:22 +0200

    many

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Footer() {
-  return <footer>THIS IS THE GOD DAMN Footer</footer>;
+  return (
+    <footer>
+      <div>
+        <i class="fa-brands fa-vimeo-v"></i>
+        <i class="fa-brands fa-youtube"></i>
+      </div>
+      <div>hello@yay.com</div>
+      <div>Copyright 2022</div>
+    </footer>
+  );
 }
 
 export default Footer;
